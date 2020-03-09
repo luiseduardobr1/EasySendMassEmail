62a9113726b8ba7ef3abe0e67d2aac6b37ce1f61
Luís Eduardo Pompeu <56649205+luiseduardobr1@users.noreply.github.com>
2020-03-09 00:21:21 -0300

    Update yagmail.py

--- yagmail.py
+++ yagmail.py
@@ -7,7 +7,7 @@ wb = load_workbook("emails-list.xlsx")  # Work Book
 ws = wb['Plan1']  # Work Sheet
 column = ws['B']  # Column
 emails = [column[x].value for x in range(len(column))] # list with all e-mails
-print('LISTA DE DESTINAT¡RIOS: ')
+print('LISTA DE DESTINAT√ÅRIOS: ')
 print(emails)
 print('\nTotal de emails a ser enviados: ' + str(len(emails)))
 
@@ -20,7 +20,7 @@ print('\nTotal de emails a ser enviados: ' + str(len(emails)))
 #yag = yagmail.SMTP('GMAIL-LOGIN', 'GMAIL-PASSWORD')
 
 # OTHER SMTP Server
-yag = yagmail.SMTP('luiseduardobr1@hotmail.com', '12345678910le', host='smtp-mail.outlook.com', port=587, smtp_starttls=True, smtp_ssl=False)
+yag = yagmail.SMTP('HOTMAIL LOGIN', 'HOTMAIL PASSWORD', host='smtp-mail.outlook.com', port=587, smtp_starttls=True, smtp_ssl=False)
 
 #--------- EMAIL MESSAGE AND ATTACHMENTS -------------------
 # Body EMAIL
@@ -28,7 +28,7 @@ contents = [
     """Hello, 
 Here is my resume.
 Sincerely,
-LuÌs Eduardo Pompeu.
+Lu√≠s Eduardo Pompeu.
 """, 'curriculo_luiseduardopompeu.pdf'
 ]
 
@@ -39,7 +39,7 @@ correct=0
 error=0
 for endereco in emails:
     try:
-        yag.send(endereco, 'Vaga Projetista - Eng. ElÈtrica', contents)
+        yag.send(endereco, 'Vaga Projetista - Eng. El√©trica', contents)
         print('Email enviado para: ' + str(endereco))
         correct=correct+1
     except:
